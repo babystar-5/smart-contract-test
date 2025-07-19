8e2f4ad80c3ad6dbbf26581b3b809201668c8df0
babystar-5 <running.c.12@gmail.com>
2025-07-19 18:14:22 +0900

    part 2

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,37 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    // ...
+
+    function buyTokens() public payable {
+        // Calculate the number of tokens to buy
+        uint tokenAmount = msg.value * rate;
+
+        // Require that EthSwap has enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        // Transfer tokens to the user
+        token.transfer(msg.sender, tokenAmount);
+
+        // Emit an event
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
 }
