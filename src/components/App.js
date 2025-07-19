7ad111019d5b3d7c01ab195e7af0aa8bbc4e598c
babystar-5 <running.c.12@gmail.com>
2025-07-19 21:28:22 +0900

    hook method

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 import Navbar from "./Navbar";
 import "./App.css";
 
-class App extends Component {
-  async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
+function App() {
+  const [account, setAccount] = useState("");
+  const [ethBalance, setEthBalance] = useState("0");
 
-  async loadBlockchainData() {
-    const web3 = window.web3;
+  useEffect(() => {
+    const init = async () => {
+      await loadWeb3();
+      await loadBlockchainData();
+    };
 
-    const accounts = await web3.eth.getAccounts();
-    this.setState({ account: accounts[0] });
+    init();
+  }, []);
 
-    const ethBalance = await web3.eth.getBalance(this.state.account);
-    this.setState({ ethBalance });
-  }
-
-  async loadWeb3() {
+  const loadWeb3 = async () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
       await window.ethereum.enable();
@@ -30,37 +27,36 @@ class App extends Component {
         "Non-Ethereum browser detected. You should consider trying MetaMask!"
       );
     }
-  }
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: "",
-      ethBalance: "0",
-    };
-  }
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                ></a>
-                <h1>Hello, World</h1>
-              </div>
-            </main>
-          </div>
+    const balance = await web3.eth.getBalance(accounts[0]);
+    setEthBalance(balance);
+  };
+
+  return (
+    <div>
+      <Navbar account={account} />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mr-auto ml-auto">
+              <a
+                href="http://www.dappuniversity.com/bootcamp"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+              <h1>Hello, World</h1>
+            </div>
+          </main>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
